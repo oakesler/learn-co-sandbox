beb84d4df278ea6f45ca46ad26a7693cfe1ec5d3
Olivia Kesler <oakesler@protonmail.ch>
2019-11-15 19:00:16 +0000

    retooled randomizer chain

--- awoke-cli/lib/methods.rb
+++ awoke-cli/lib/methods.rb
@@ -2,136 +2,105 @@ story_object_array = [ ]
 source_array = [ ]
 
 def welcome_menu
-	puts    “Welcome to Awoke! Select by story, news source, or use our randomizer.”
-	puts    “For story selection, enter ‘story’”
-	puts    “For source selection, enter ‘source’”
-	puts    “For randomizer, enter ‘random’”
-	puts    “To exit, enter ‘exit’”
+	puts "Welcome to Awoke! Select by story, news source, or use our randomizer."
+	puts "For story selection, type ‘story'"
+	puts "For source selection, type ‘source'"
+	puts "For randomizer, type ‘random'"
+	puts "To exit, type ‘exit’"
 	input_1 = gets.strip
 	if input_1 == “exit”
 		puts “Thanks for using Awoke!”
-	else
+	elsif input_1 == "story" || input_1 == "source" || input_1 == "random"
 		story_generator(input_1)
+		else 
+		  menu_redirect
 	end 
 end
 
 def story_generator(input_1)
-  if input_1 == “story”
+  if input_1 == "story"
     story_printer
-    elsif input_1 == “source”
+    elsif input_1 == "source"
     source_printer
-		puts “1. #{Source 1}”
-		puts “2. #{Source 2}”
-		puts “3. #{Source 3}”
-		puts “Please select a source by number (1, 2, 3)”
-		input_3 = gets.strip
-		if input_3.to_i (1, 3)
-		  source_array.each do |item|
-		    if item[0] == ("#{input_3}")
-		      puts item
-		      end_story_prompt
-		  end
-		end
+    elsif input_1 == "random"
+    randomizer
+  else
+    menu_redirect
+	end
+end
 		
-	def story_printer 
-			puts “Story 1”
-		puts “Story 2”
-		puts “Story 3”
-		puts “Please select a story by number (1, 2, 3)”
-		input_2 = gets.strip
-		if input_2.to_i (1, 3)
-		  story_text_array.each do |item|
-		    if item[0] == ("#{input_2}")
-		      puts item
+def story_printer
+  puts "#{Story 1}"
+	puts "#{Story 2}"
+	puts "#{Story 3}"
+	puts "Please select a story by number (1, 2, 3)"
+	input_2 = gets.strip
+	if input_2.to_i (1, 3)
+		story_text_array.each do |item|
+		  if item[0] == ("#{input_2}")
+		    puts item
 		    end_story_prompt
 		  end
+		else
+		  menu_redirect
 		end
 	end
+end
 	
-	def source_printer
-		puts “1. #{Source 1}”
-		puts “2. #{Source 2}”
-		puts “3. #{Source 3}”
-		puts “Please select a source by number (1, 2, 3)”
-		input_3 = gets.strip
-		if input_3.to_i (1, 3)
-		  source_array.each do |item|
-		    if item[0] == ("#{input_3}")
-		      puts item
-		      end_story_prompt
-		    end
+def source_printer
+	puts “1. #{Source 1}”
+	puts “2. #{Source 2}”
+	puts “3. #{Source 3}”
+	puts “Please select a source by number (1, 2, 3)”
+	input_3 = gets.strip
+	if input_3.to_i (1, 3)
+		source_array.each do |item|
+		  if item[0] == ("#{input_3}")
+		    puts item
+		    end_story_prompt
 		  end
+		else 
+		  menu_redirect
 		end
 	end
+end
 	
-	def randomizer 
-	  index_array = [ ]
-	  y = 0 
-	  while y <= story_object_array.length - 1 
-	    index_array << y 
-	  end
-	  z = index_array.sample
-	  puts story_object_array[z].name
-	  story_object_array.delete(z)
-	  puts "To continue reading, type continue"
-	  puts "To generate a new story, type new"
-	  input_7 = gets.strip
-	  if input_7 == "continue"
-	    puts story_object_array[z].text
-	    end_story_prompt
-	    elsif input_7 == "new"
-	    randomizer
+def index_array_generator 
+	 index_array = [ ]
+	 y = 0 
+	 while y <= story_object_array.length - 1
+	  index_array << y 
+	  y += 1
+	 end
+	 randomizer(index_array)
+	end
+	
+def randomizer(array)
+  z = array.sample
+  story = story_object_array[z].name
+  array.delete(z)
+  randomizer_selector(story)
+end
+	
+def randomizer_selector(story)
+  puts "To continue reading, type 'continue'"
+	puts "To generate a new story, type 'new'"
+	input_7 = gets.strip
+	if input_7 == "continue"
+	  puts "#{story_object_array[z].author}"
+	  puts "#{story_object_array[z].text}"
+	  end_story_prompt
+	  elsif input_7 == "new"
+	  x = index_array.sample
+	    puts story_object_array[x]
+	    
 	    else
 	      welcome_menu
 	    end 
 	  end
 	end
-	    
-	    
-	  
 	
-	
-		
-		
-		
-		
-		
-		#if input_2 == 1
-			#puts “1. #{Story 1}”
-			#end_story_prompt
-			#elsif input_2 == 2
-			#puts “2. #{Story 2}”
-			#end_story_prompt
-			#elsif input_2 == 3
-			#puts “3. #{Story 3}”
-			#end_story_prompt
-	elsif input_1 == “source”
-		puts “1. #{Source 1}”
-		puts “2. #{Source 2}”
-		puts “3. #{Source 3}”
-		puts “Please select a source by number (1, 2, 3)”
-		input_3 = gets.strip
-		if input_3 == “1”
-			puts “1. #{Source 1}”
-			end_story_prompt
-		elsif input_3 == “2”
-			puts “2. #{Source 2}”
-			end_story_prompt
-		elsif input_3 == “3”
-			puts “3. #{Source 3}”
-			end_story_prompt
-	elsif input_1 == “random”
-		puts “#{story object}”
-		puts “View story or regenerate?”
-		input_4 = gets.strip
-		if input_4 == “View story”
-			puts “#{story_object.text}
-		elsif input_4 == “regenerate”
-			puts “#{story object}
-	else
-		puts “Invalid input, returning to main menu”
-		welcome_menu
-		end
+	def menu_redirect
+	  puts "Sorry, we don't recognize that input. Returning to main menu..."
+	  welcome_menu
 	end
-end
-end
--- awoke-cli/lib/story_object.rb
+++ awoke-cli/lib/story_object.rb
@@ -3,13 +3,19 @@ class Story
   attr_accessor :name, :text, :source, :author
   
   @@story_object_array = [ ]
+  @@source_array = [ ]
   def initialize(name, text, source, author = "none")
     @name = name 
     @text = text 
     @source = source
+    @author = author
     if @author == "none"
       @author = "#{source}"
-      @@story_object_array << self
     end
-  end 
+    if !@@source_array.include?(self.source)
+      @@source_array << self.source
+    end
+    @@story_object_array << self
+  end
 end
+

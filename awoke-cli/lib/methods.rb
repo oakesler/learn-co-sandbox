53e356a71835764fa8c95749cd9d36bdaafa61d3
Olivia Kesler <oakesler@protonmail.ch>
2019-12-03 18:57:57 +0000

    retooled aclu url scrape

--- awoke-cli/lib/methods.rb
+++ awoke-cli/lib/methods.rb
@@ -6,6 +6,8 @@ require 'nokogiri'
 require 'open-uri'
 @story_hash = {:ACLU => " " , :Amnesty => " ", :HRW => " " , :SPLC => " ", :Backup => " "}
 
+binding.pry
+
 def the_aclu_headline_scraper
   html_aclu = open("https://www.aclu.org")
   doc_aclu = Nokogiri::HTML(html_aclu)
@@ -35,12 +37,15 @@ end
 def the_aclu_url_scraper
   html_aclu = open("https://www.aclu.org")
   doc_aclu = Nokogiri::HTML(html_aclu)
-  step_a_1 = doc_aclu.css("div#hp__top_carousel")
-  step_a_2 = step_a_1.css("div.columns")
-  step_a_3 = step_a_2.children
-  step_a_4 = step_a_3[11]
-  step_a_5 = step_a_4.css("a").first
-  aclu_url = step_a_5.attributes["href"].value
+  step_1 = doc_aclu.css("div#hp__top_spotlight")
+  step_2 = step_1.css("a")[0].to_a
+  aclu_url = step_2[0][1]
+  #step_a_1 = doc_aclu.css("div#hp__top_carousel")
+  #step_a_2 = step_a_1.css("div.columns")
+  #step_a_3 = step_a_2.children
+  #step_a_4 = step_a_3[11]
+  #step_a_5 = step_a_4.css("a").first
+  #aclu_url = step_a_5.attributes["href"].value
 end
 
 def the_amnesty_url_scraper 
@@ -92,80 +97,6 @@ def the_splc_abstract_scraper
   splc_abstract = doc_splc.css("p").first.text
 end
 
-
-
-#####################################################################################################
-#def aclu_abstract_scraper
-  #html_aclu = open("#{@story_hash[:ACLU].story_url}")
-  #doc_aclu = Nokogiri::HTML(html_aclu)
-  #aclu_abstract = doc_aclu.css("div#tabs").text
-#end
-
-#def amnesty_headline_scraper
-  #html_amnesty = open("https://www.amnesty.org/en/")
-  #doc_amnesty = Nokogiri::HTML(html_amnesty)
-  #headline_amnesty = "#{doc.css('span.heading--tape').text} : #{doc.css('p.image-headline__copy')}"
-  #headline_finisher(headline_amnesty, "Amnesty International")
-#end
-
-#def amnesty_url_scraper 
-  #html_amnesty = open("https://www.amnesty.org/en")
-  #doc_amnesty = Nokogiri::HTML(html_amnesty)
-  #step_b_1 = doc_amnesty.css("a.btn--header")
-  #amnesty_url = "https://www.amnesty.org/#{step_b_1}"
-#end 
-
-#def amnesty_abstract_scraper
-  #html_amnesty = open("#{@story_hash[:Amnesty].story_url}")
-  #doc_amnesty = Nokogiri::HTML(html_amnesty)
-  #amnesty_abstract = doc_amnesty.css("p").text
-#end
-
-#def hrw_headline_scraper
-  #html_hrw = open("https://www.hrw.org/#")
-  #doc_hrw = Nokogiri::HTML(html_hrw)
-  #headline_hrw = doc.css('h3.billboard-title')
-  #headline_finisher(headline_hrw, "HRW")
-#end
-
-#def hrw_url_scraper
-  #html_hrw = open("https://www.hrw.org")
-  #doc_hrw = Nokogiri::HTML(html_hrw)
-  #hrw_url = "https://www.hrw.org#{doc_hrw.css("h3.billboard-title a").map { |link| link["href"] }[0]}"
-  #@url_hash[:HRW] = hrw_url
-#end
-
-#def hrw_abstract_scraper
-  #html_hrw = open("#{@story_hash[:HRW].story_url}")
-  #doc_hrw = Nokogiri::HTML(html_hrw)
-  #step_c_1 = doc_hrw.css("p")
-  #hrw_abstract = step_c_1[3]
-#end
-
-#def splc_headline_scraper
-  #html_splc = open("https://www.splcenter.org")
-  #doc_splc = Nokogiri::HTML(html_splc)
-  #headline_splc = doc_splc.css("div.field-item even")
-  #headline_finisher(headline_hrw, "SPLC")
-#end
-
-#def splc_url_scraper
-  #html_splc = open("https://www.splcenter.org")
-  #doc_splc = Nokogiri::HTML(html_splc)
-  #step_1 = doc_splc.css("section#highlighted")
-  #step_2 = step_1.css("div.field-items")
-  #step_3 = step_2[0].children
-  #step_4 = step_3[1].children.text
-  #@url_hash[:SPLC] = "#{step_4.match(/https.*\w/)}"
-#end
-
-#def splc_abstract_scraper
-  #html_splc = open("#{@story_hash[:SPLC].story_url}")
-  #doc_splc = Nokogiri::HTML(html_splc)
-  #splc_abstract = doc_splc.css("p").first.text
-#end
-
-######################################################################################
 def aclu_object_maker
   aclu_story = Story.new("ACLU", "www.aclu.org")
   aclu_story.headline = the_aclu_headline_scraper
@@ -240,8 +171,6 @@ def story_selector_segue
   end
 end
 
-
-
 def story_selector
   puts "Please select a headline by number (1-4)"
   puts "1. #{@story_hash[:ACLU].headline}"
@@ -297,7 +226,7 @@ end
 def randomizer
   sample_array = [1, 2, 3, 4]
   x = sample_array.sample
-  if x == "1"
+  if x == 1
     puts "*****************************"
     puts "#{@story_hash[:ACLU].source} (#{@story_hash[:ACLU].home_url})"
     puts "*****************************"
@@ -306,8 +235,9 @@ def randomizer
     puts "#{@story_hash[:ACLU].story_url}"
     puts "*****************************"
     puts "#{@story_hash[:ACLU].abstract}"
+    puts "*****************************"
     story_selector_segue
-    elsif x == "2"
+    elsif x == 2
     puts "*****************************"
     puts "#{@story_hash[:Amnesty].source} (#{@story_hash[:Amnesty].home_url})"
     puts "*****************************"
@@ -316,8 +246,9 @@ def randomizer
     puts "#{@story_hash[:Amnesty].story_url}"
     puts "*****************************"
     puts "#{@story_hash[:Amnesty].abstract}"
+    puts "*****************************"
     story_selector_segue
-    elsif x == "3"
+    elsif x == 3
     puts "*****************************"
     puts "#{@story_hash[:HRW].source} (#{@story_hash[:HRW].home_url})"
     puts "*****************************"
@@ -326,8 +257,9 @@ def randomizer
     puts "#{@story_hash[:HRW].story_url}"
     puts "*****************************"
     puts "#{@story_hash[:HRW].abstract}"
+    puts "*****************************"
     story_selector_segue
-    elsif x == "4"
+    elsif x == 4
     puts "*****************************"
     puts "#{@story_hash[:SPLC].source} (#{@story_hash[:SPLC].home_url})"
     puts "*****************************"
@@ -336,6 +268,7 @@ def randomizer
     puts "#{@story_hash[:SPLC].story_url}"
     puts "*****************************"
     puts "#{@story_hash[:SPLC].abstract}"
+    puts "*****************************"
     story_selector_segue
   else 
     story_selector_segue
